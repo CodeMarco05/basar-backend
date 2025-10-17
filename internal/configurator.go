1ee26d3fd2d49cef0118fa40074f7b1c749ada58
Marco <brandt.marco05@gmail.com>
2025-10-17 17:26:35 +0000

    health and auth plus logger

--- internal/configurator.go
+++ internal/configurator.go
@@ -3,6 +3,7 @@ package internal
 import (
 	"encoding/json"
 	"fmt"
+	"hackathon-basar-backend/internal/server/routes"
 	"net/http"
 	"os"
 	"strconv"
@@ -20,6 +21,8 @@ type AppConfig struct {
 var Config *AppConfig
 
 func LoadApplicationConfig() {
+	// ------------------------------------------------------------ //
+	// load the env config
 	logger := GetLogger()
 
 	err := godotenv.Load()
@@ -66,9 +69,16 @@ func ChiConfig() *chi.Mux {
 
 	// TODO implement own logger middle ware
 
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(AuthMiddleware)
+	r.Get("/health", routes.HealthCheck)
+
 	r.Route("/api/v1", func(r chi.Router) {
+		r.Use(middleware.Logger)
 		r.Use(middleware.Recoverer)
 		r.Use(AuthMiddleware)
+
 	})
 
 	return r
--- internal/server/routes/v1/auth.go
+++ internal/server/routes/v1/auth.go
@@ -1,1 +1,7 @@
 package v1
+
+import "net/http"
+
+func CheckAuthentication(w http.ResponseWriter, r *http.Request) {
+
+}
