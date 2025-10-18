3b3c850254ceb518b3c8864d593c05a5a18b7f87
Marco <brandt.marco05@gmail.com>
2025-10-18 10:57:19 +0000

    added mail

--- internal/models/post.go
+++ internal/models/post.go
@@ -11,6 +11,7 @@ type Post struct {
 	Description string    `bson:"description" json:"description"`
 	Tags        []string  `bson:"tags" json:"tags"`
 	Text        string    `bson:"text" json:"text"`
+	Mail        string    `bson:"mail" json:"mail"`
 	PayPalMail  string    `bson:"payPalMail" json:"payPalMail"`
 	Images      []string  `bson:"images" json:"images"`
 	CreatedAt   time.Time `bson:"created_at" json:"created_at"`
@@ -22,6 +23,7 @@ type InsertPost struct {
 	Description string   `bson:"description" json:"description" validate:"required"`
 	Tags        []string `bson:"tags" json:"tags" validate:"required"`
 	Text        string   `bson:"text" json:"text" validate:"required"`
+	Mail        string   `bson:"mail" json:"mail" validate:"required"`
 	PayPalMail  string   `bson:"payPalMail" json:"payPalMail" validate:"required,email"`
 	Images      []string `bson:"images" json:"images" validate:"required"`
 }
